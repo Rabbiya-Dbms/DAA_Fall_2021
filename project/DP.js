43ec1e3069297f40ec37c2f8884a61fdb82c8a13
Misbah Shakeel <61609624+github373@users.noreply.github.com>
2022-01-06 18:52:23 +0600

    Update DP.js

--- project/DP.js
+++ project/DP.js
@@ -11,3 +11,19 @@ const Constraints = (rows, col, sol) => {
     }
     return true;
 }
+
+
+
+//Verification. Assume that S is the set of closed lines and that m is the number of queens put by C.
+//Let C be the result of completing C. Because C and C both have the same number of queens,
+//adjoining C to C will result in a candidate with n queens, according to assumption,
+//C did not place a queen on any line in S?, which consisted of the closed lines in C. 
+//As a result of adjoining C, completing C, to C, the result is a solution.
+
+// What we discovered is that
+//We have defined its 0 (f (n> 8n) algorithm
+//problem n-queens. There is some evidence
+//that number of solutions to the problem
+//super-exponential . If this is true, then ours
+//The algorithm is superior to any method (e.g.
+//backtracking) clearly forms all problematic solutions.
